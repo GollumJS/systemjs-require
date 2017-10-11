33d1d07c390fde4bcbf5b690840c0087382c3e78
SmeagolWorms4 <smeagolworms4@gmail.com>
2017-10-11 13:37:18 +0200

    Initial commit

---
+++ systemjs-require.js
@@ -0,0 +1,38 @@
+(function(window, System, SystemJS) {
+	var modules = {};
+	
+	window.require = function(key) {
+		if (!modules[key]) {
+			throw new Error('Module '+key+' not found.');
+		}
+		return modules[key];
+	};
+	
+	var origin   = System.register;
+	var originJS = SystemJS.register;
+	
+	System.register = function(key) {
+		origin.apply(System, arguments);
+		setTimeout(function() {
+			System
+				.import(key)
+				.then(function(module) {
+					modules[key] = module;
+				}, console.error.bind(console))
+			;
+		});
+	};
+	
+	SystemJS.register = function(key) {
+		originJS.apply(SystemJS, arguments);
+		setTimeout(function() {
+			System
+				.import(key)
+				.then(function (module) {
+					modules[key] = module;
+				}, console.error.bind(console))
+			;
+		});
+	};
+	
+})(window, System, SystemJS);
